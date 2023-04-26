1d139b68c85f0d0c5be4a68a40e8c4aa7ab07f16
sujoymodakgithub <modaksujoy74@gmail.com>
2023-04-26 02:32:24 +0000

    Checkbox with Label

---
+++ index.js
@@ -0,0 +1,17 @@
+let checkboxWithLabelContainerE1 = document.getElementById(
+  "checkboxWithLabelContainer"
+);
+
+checkboxWithLabelContainerE1.classList.add("text-center", "p-5");
+
+let checkBoxE1 = document.createElement("input");
+checkBoxE1.type = "checkbox";
+checkBoxE1.id = "checkbox";
+checkboxWithLabelContainerE1.appendChild(checkBoxE1);
+
+let labelE1 = document.createElement("label");
+labelE1.setAttribute("for", "checkbox");
+labelE1.textContent = "Click Me!";
+labelE1.classList.add("ml-2");
+labelE1.id = "checkboxLabel";
+checkboxWithLabelContainerE1.appendChild(labelE1);
